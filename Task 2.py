863578f16338bfb9bbe461ba447c5412744725ba
Maheshvar V <maheshvarkv007@gmail.com>
2025-12-04 16:42:48 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,23 @@
+#Simple Calculator Program
+#Task 2
+def calculator(n1,n2,oper):
+    if oper=='+':
+        return n1+n2
+    elif oper=='-':
+        return n1-n2
+    elif oper=='*':
+        return n1*n2
+    elif oper=='/':
+        return n1/n2
+    elif oper=='//':
+        return n1//n2
+    elif oper=='%':
+        return n1%n2
+    else:
+        print('Invalid operator..')
+    return 0
+num1=float(input('Enter first number:'))
+num2=float(input('Enter second number:'))
+op=input('Input operator among +,-,*,/,//,%:')
+print(calculator(num1,num2,op))
+
