abcb99b9f4693498573fc5942f2abd3a9cc8b552
Maheshvar V <maheshvarkv007@gmail.com>
2025-12-15 12:32:22 +0530

    Implement error handling for division by zero
    Added division by zero error handling to the calculator function.

--- Task 2.py
+++ Task 2.py
@@ -8,6 +8,8 @@ def calculator(n1,n2,oper):
     elif oper=='*':
         return n1*n2
     elif oper=='/':
+        if(n2==0):
+            print("Division By Zero Error!!")
         return n1/n2
     elif oper=='//':
         return n1//n2
@@ -20,4 +22,5 @@ num1=float(input('Enter first number:'))
 num2=float(input('Enter second number:'))
 op=input('Input operator among +,-,*,/,//,%:')
 print(calculator(num1,num2,op))
+
 
